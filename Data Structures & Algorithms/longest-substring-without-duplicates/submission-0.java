224d6af00ac683d545537ddc49fa1bdeffaf3e3c
akshatt-24 <heyakshatt@gmail.com>
2026-05-28 01:19:53 +0530

    Bulk sync: 19 submissions

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        // brute force
+
+        int res = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            Set<Character> set = new HashSet<>();
+
+            for(int j = i; j < s.length() ; j++) {
+                if(set.contains(s.charAt(j))) break;
+                set.add(s.charAt(j));
+            }
+            res = Math.max(res, set.size());
+        }
+        return res;
+    }
+}
